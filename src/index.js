b96c8b853dafecb6a81f688692fb6d6b6bfadf85
Manthan Mallikarjun <nahtnam@gmail.com>
2018-04-05 12:11:00 -0700

    add live-reload to #1 and set it to off by default

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const chokidar = require('chokidar');
 
+const currentDirectory = process.cwd();
+
 module.exports = class marko {
   constructor(options = {}) {
-    this.liveReload = options.liveReload || true;
-    this.writeToDisk = options.writeToDisk || false;
+    // currently set to false until bug fixes are resolved
+    this.liveReload = (typeof options.liveReload === 'boolean') ? options.liveReload : false;
+    this.writeToDisk = (typeof options.writeToDisk === 'boolean') ? options.writeToDisk : false;
+    this.sourceFolder = (typeof options.sourceFolder === 'string') || './src';
+    this.hotReloadDir = (typeof options.hotReloadDir === 'string') || path.join(currentDirectory, this.sourceFolder, './**/*.marko');
+    this.watchDirs = [currentDirectory, path.join(currentDirectory, './assets/js'), path.join(currentDirectory, './assets/css')];
+    this.watchServer = (typeof options.watchServer === 'object') || {
+      extraExts: ['marko'],
+    };
   }
 
   setup(app) {
@@ -17,10 +26,13 @@ module.exports = class marko {
     if (this.liveReload) {
       const hotReload = require('marko/hot-reload');
       hotReload.enable();
-      const currentDirectory = process.cwd();
-      chokidar.watch(`${currentDirectory}/**/*.marko`).on('change', (file) => {
+      chokidar.watch(this.hotReloadDir).on('change', (file) => {
         hotReload.handleFileModified(file);
       });
+
+      const livereload = require('livereload');
+      const lrserver = livereload.createServer(this.watchServer);
+      lrserver.watch(this.watchDirs);
     }
 
     app.render = async (templatePath, data) => {
@@ -35,20 +47,23 @@ module.exports = class marko {
       let templatePath;
       if (args.length === 4) {
         [templatePath, data, ctx, appObj] = args;
-        templatePath = path.join(path.join(process.cwd(), ctx.route), templatePath);
+        templatePath = path.join(
+          path.join(process.cwd(), this.sourceFolder, ctx.route),
+          templatePath,
+        );
       }
       if (args.length === 3) {
         [data, ctx, appObj] = args;
         data = data || {};
-        templatePath = path.join(process.cwd(), ctx.route, './index.marko');
+        templatePath = path.join(process.cwd(), this.sourceFolder, ctx.route, './index.marko');
       }
       if (args.length === 2) {
         [ctx, appObj] = args;
-        templatePath = path.join(process.cwd(), ctx.route, './index.marko');
+        templatePath = path.join(process.cwd(), this.sourceFolder, ctx.route, './index.marko');
       }
-
+      const injectedData = Object.assign({}, ctx.state, data);
       ctx.type = 'html';
-      return appObj.render(templatePath, data);
+      return appObj.render(templatePath, injectedData);
     };
   }
 };
